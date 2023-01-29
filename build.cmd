f30aa30954ec6d992df01859c3c42b79b1412ecf
sloow001 <marcosrmpires@gmail.com>
2023-01-29 15:31:57 -0300

    simple template

---
+++ build.cmd
@@ -0,0 +1,6 @@
+@echo off
+cls
+color 02
+echo building the game...
+love .
+pause
---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.window.title = "love2d template"
+    t.window.width = 1280
+    t.window.height = 720
+end
